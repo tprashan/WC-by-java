41d7e37f48d946dfaa6356052b66eef8b729413e
unknown <tprashan@INTPRASHAN.corporate.thoughtworks.com>
2015-02-20 18:48:02 +0530

    In this version of wc handled multi option(flags) but not arranged.

--- HandleMultipleOptions.java
+++ HandleMultipleOptions.java
@@ -8,21 +8,20 @@ class HandleMultipleOptions{
       }
       public void handleMultiOptionAndMultiFiles(){
             wc wcObject = new wc();
-            int i;int noOfFiles = file.length;
+            int i,j;int noOfFiles = file.length;
             for(i=0;i<noOfFiles;i++){
                   if(file[i]!=null){
-                        String fileContent = ReadFile.read(file[i]);
-                        int count[] =  wcObject.lines_words_chars(fileContent);
-                        PerformOption perOpt = new PerformOption();
-                        perOpt.handleOption(flag[0],count[0],count[1],count[2],file[i]);
+                        for(j=0;j<noOfFiles;j++){
+                              if(flag[j]!=null){
+                                    String fileContent = ReadFile.read(file[i]);
+                                    int count[] =  wcObject.lines_words_chars(fileContent);
+                                    PerformOption perOpt = new PerformOption();
+                                    perOpt.handleOption(flag[j],count[0],count[1],count[2],file[i]);
+                              }
+                        }
                   }
                   
             }
       }
 }
 
-// String fileContent = ReadFile.read(file[0]);
-// int count[] =  wcObject.lines_words_chars(fileContent);
-
-// PerformOption perOpt = new PerformOption();
-// perOpt.handleOption(flag[0],
